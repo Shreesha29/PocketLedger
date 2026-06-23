6fa41dde72b1a031478e840cbf27e61cc4afc71c
Shreesha29 <shrishajois@gmail.com>
2026-06-23 21:38:19 +0530

    feat(domain): add category entity

---
+++ PocketLedger.Domain/Entities/Category.cs
@@ -0,0 +1,38 @@
+﻿using PocketLedger.Domain.Common;
+using PocketLedger.Domain.Enums;
+
+namespace PocketLedger.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        private Category()
+        {
+        }
+
+        public Category(
+            Guid userId,
+            string name,
+            CategoryType type)
+        {
+            UserId = userId;
+            Name = name;
+            Type = type;
+        }
+
+        public Guid UserId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public CategoryType Type { get; private set; }
+
+        public void Update(
+            string name,
+            CategoryType type)
+        {
+            Name = name;
+            Type = type;
+
+            SetUpdatedAt();
+        }
+    }
+}
--- PocketLedger.Infrastructure/obj/Debug/net8.0/PocketLedger.Infrastructure.AssemblyInfo.cs
+++ PocketLedger.Infrastructure/obj/Debug/net8.0/PocketLedger.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketLedger.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcb83d882d85c4895ea485e5c9c3cc51f386e12")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketLedger.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketLedger.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
