5938f67971fdc4c9e05804c02c9249c873f99263
Shreesha29 <shrishajois@gmail.com>
2026-06-25 20:42:41 +0530

    Added Budget and Transaction Entities

---
+++ PocketLedger.Domain/Entities/Budget.cs
@@ -0,0 +1,12 @@
+﻿using PocketLedger.Domain.Common;
+
+namespace PocketLedger.Domain.Entities
+{
+    public class Budget : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public decimal BudgetAmount { get; set; }
+    }
+}
---
+++ PocketLedger.Domain/Entities/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using PocketLedger.Domain.Common;
+
+namespace PocketLedger.Domain.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        private Transaction()
+        {
+
+        }
+
+        public Transaction(Guid userId, Guid categoryId, decimal amount, string? description, DateTime transactionDate)
+        {
+            UserId = userId;
+            CategoryId = categoryId;
+            Amount = amount;
+            Description = description;
+            TransactionDate = transactionDate;
+        }
+
+        public Guid UserId { get; private set; }
+        public Guid CategoryId { get; private set; }
+
+        public decimal Amount { get; private set; }
+
+        public string? Description { get; private set; }
+
+        public DateTime TransactionDate { get; private set; }
+
+        public void Update(
+            Guid categoryId,
+            decimal amount,
+            string? description,
+            DateTime transactionDate)
+        {
+            CategoryId = categoryId;
+            Amount = amount;
+            Description = description;
+            TransactionDate = transactionDate;
+
+            SetUpdatedAt();
+        }
+    }
+}
